54947a9469a58b22152a1d19df3ed298d8d5e071
bhavik doshi <bhavikdoshi225@gmail.com>
2021-08-23 23:40:38 +0530

    first mern app

---
+++ client/src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+
+const Home = () => {
+    const [userName, setUserName] = useState('');
+    const [show, setShow] = useState(false);
+
+    const userHomePage = async () => {
+        try {
+            const res = await fetch('/getdata', {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+
+            const data = await res.json();
+            // console.log(data);
+            setUserName(data.name);
+            setShow(true);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        userHomePage();
+    }, []);
+    return (
+        <>
+            <div className="home-page">
+                <div className="home-div">
+                    <h1 className="pt-5">I AM BHAVIK DOSHI WELCOME TO MY PAGE</h1>
+                    <h1>{userName}</h1>
+                    <h2> {show ? 'Happy, to see you back' : ' I AM MERN Developer'}</h2>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Home
