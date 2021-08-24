7edc794e999d899764d5dd9113540a589e19f71e
bhavik doshi <bhavikdoshi225@gmail.com>
2021-08-24 14:50:51 +0530

    modifyed

--- router/auth.js
+++ router/auth.js
@@ -8,9 +8,9 @@ const authenticate = require("../middleware/authenticate");
 require('../db/conn');
 const User = require("../model/userSchema");
 
-router.get('/', (req, res) => {
-    res.send(`Hello world from the server rotuer js`);
-});
+// router.get('/', (req, res) => {
+//     res.send(`Hello world from the server rotuer js`);
+// });
 
 
 
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
 // router.post('/register', (req, res) => {
 
 //     const { name, email, phone, work, password, cpassword} = req.body;
-    
+
 //     if (!name || !email || !phone || !work || !password || !cpassword) {
 //         return res.status(422).json({ error: "Plz filled the field properly" });
 //     }
@@ -29,13 +29,13 @@ router.get('/', (req, res) => {
 //             if (userExist) {
 //                 return res.status(422).json({ error: "Email already Exist" });
 //             }
-            
+
 //             const user = new User({ name, email, phone, work, password, cpassword });
 
 //             user.save().then(() => {
 //                 res.status(201).json({ message: "user registered successfuly" });
 //             }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
-            
+
 //         }).catch(err => { console.log(err); });
 
 // });
@@ -44,8 +44,8 @@ router.get('/', (req, res) => {
 
 router.post('/register', async (req, res) => {
 
-    const { name, email, phone, work, password, cpassword} = req.body;
-    
+    const { name, email, phone, work, password, cpassword } = req.body;
+
     if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ error: "Plz filled the field properly" });
     }
@@ -55,17 +55,17 @@ router.post('/register', async (req, res) => {
         const userExist = await User.findOne({ email: email });
 
         if (userExist) {
-             return res.status(422).json({ error: "Email already Exist" });
+            return res.status(422).json({ error: "Email already Exist" });
         } else if (password != cpassword) {
-             return res.status(422).json({ error: "password are not matching" });
+            return res.status(422).json({ error: "password are not matching" });
         } else {
-             const user = new User({ name, email, phone, work, password, cpassword });
+            const user = new User({ name, email, phone, work, password, cpassword });
             // yeha pe 
             await user.save();
             res.status(201).json({ message: "user registered successfuly" });
         }
-        
-  
+
+
     } catch (err) {
         console.log(err);
     }
@@ -80,7 +80,7 @@ router.post('/signin', async (req, res) => {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return res.status(400).json({error:"Plz Filled the data"})
+            return res.status(400).json({ error: "Plz Filled the data" })
         }
 
         const userLogin = await User.findOne({ email: email });
@@ -90,24 +90,24 @@ router.post('/signin', async (req, res) => {
         if (userLogin) {
             const isMatch = await bcrypt.compare(password, userLogin.password);
 
-           
 
-        if (!isMatch) {
+
+            if (!isMatch) {
+                res.status(400).json({ error: "Invalid Credientials " });
+            } else {
+                // need to genereate the token and stored cookie after the password match 
+                token = await userLogin.generateAuthToken();
+                console.log(token);
+
+                res.cookie("jwtoken", token, {
+                    expires: new Date(Date.now() + 25892000000),
+                    httpOnly: true
+                });
+
+                res.json({ message: "user Signin Successfully" });
+            }
+        } else {
             res.status(400).json({ error: "Invalid Credientials " });
-        } else {
-             // need to genereate the token and stored cookie after the password match 
-            token = await userLogin.generateAuthToken();
-            console.log(token);
-
-            res.cookie("jwtoken", token, {
-                expires: new Date(Date.now() + 25892000000),
-                httpOnly:true
-            });
-            
-            res.json({ message: "user Signin Successfully" });
-        }
-        } else {
-             res.status(400).json({ error: "Invalid Credientials " });
         }
 
     } catch (err) {
@@ -118,7 +118,7 @@ router.post('/signin', async (req, res) => {
 
 // about us ka page 
 
-router.get('/about', authenticate ,(req, res) => {
+router.get('/about', authenticate, (req, res) => {
     console.log(`Hello my About`);
     res.send(req.rootUser);
 });
@@ -135,7 +135,7 @@ router.post('/contact', authenticate, async (req, res) => {
     try {
 
         const { name, email, phone, message } = req.body;
-        
+
         if (!name || !email || !phone || !message) {
             console.log("error in contact form");
             return res.json({ error: "plzz filled the contact form " });
@@ -144,7 +144,7 @@ router.post('/contact', authenticate, async (req, res) => {
         const userContact = await User.findOne({ _id: req.userID });
 
         if (userContact) {
-            
+
             const userMessage = await userContact.addMessage(name, email, phone, message);
 
             await userContact.save();
@@ -152,7 +152,7 @@ router.post('/contact', authenticate, async (req, res) => {
             res.status(201).json({ message: "user Contact successfully" });
 
         }
-        
+
     } catch (error) {
         console.log(error);
     }
